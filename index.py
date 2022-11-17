660bb098c3089e8336e7fd580c0a08fd4b2fdc13
RobertoLocatelli02 <rlocatelli02@gmai.com>
2022-11-17 19:12:56 -0300

    WIP

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 import sqlite3
 
 app = Flask(__name__)
@@ -16,6 +16,28 @@ def db_connection():
                 aContato text,
                 aProfissao text,
                 aArea text
+            );
+        '''
+        cursor.execute(sql_query)
+
+        sql_query = '''
+            CREATE TABLE IF NOT EXISTS tblRequisicoes (
+                bID integer PRIMARY KEY,
+                bTitulo text,
+                bDataExpectativa text,
+                bDescricao text,
+                bObservacoes text
+            );
+        '''
+        cursor.execute(sql_query)
+
+        sql_query = '''
+            CREATE TABLE IF NOT EXISTS tblAcompanhamento (
+                cID integer PRIMARY KEY,
+                cRequisicaoID text,
+                cUsuarioID text,
+                cStatus text,
+                cData text
             );
         '''
         cursor.execute(sql_query)
@@ -53,7 +75,291 @@ def cadastroUsuario():
     conn.execute(sql_query, (nome, email, contato, profissao, area))
     conn.commit()
 
-    return request.form
+    response_data = jsonify({
+        'title': "Confirmação",
+        'body': "<div class='text-center text-success'><i class='fas fa-check fa-4x mb-3'></i></div><div class='text-black'>Usuario cadastrado com sucesso</div>"
+     })
+
+    return make_response(response_data, 200)
+
+
+@app.route('/dadosUsuario', methods = ['POST'])
+def dadosUsuario():
+    conn = db_connection()
+    cursor = conn.cursor()
+    usuarioID = request.get_json()['txtEditUsuarioID']
+
+    sql_query = f"SELECT * FROM tblUsuarios WHERE aID = {usuarioID}"
+    cursor = conn.execute(sql_query)
+    usuario = cursor.fetchone()
+
+    response_data = jsonify({
+        'id': usuario[0],
+        'nome': usuario[1],
+        'email': usuario[2],
+        'telefone': usuario[3],
+        'profissao': usuario[4],
+        'area': usuario[5]
+    })
+
+    resp = make_response(response_data, 200)
+    return resp
+
+@app.route('/editarUsuario', methods = ['POST'])
+def editarUsuario():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    id = request.form.get('txtEditusuarioID')
+    nome = request.form.get('txtEditarNome')
+    email = request.form.get('txtEditarEmail')
+    contato = request.form.get('txtEditarContato')
+    profissao = request.form.get('txtEditarProfissao')
+    area = request.form.get('txtEditarArea')
+
+    sql_query = f"UPDATE tblUsuarios SET aNome = '{nome}', aEmail = '{email}', aContato = '{contato}', aProfissao = '{profissao}', aArea = '{area}' WHERE aID = {id}"
+    conn.execute(sql_query)
+    conn.commit()
+
+    response_data = jsonify({
+        'title': "Confirmação",
+        'body': "<div class='text-center text-success'><i class='fas fa-check fa-4x mb-3'></i></div><div class='text-black'>Usuario modificado com sucesso</div>"
+     })
+
+    return make_response(response_data, 200)
+
+@app.route('/deletarUsuario', methods = ['POST'])
+def deletarUsuario():
+    conn = db_connection()
+    cursor = conn.cursor()
+    usuarioID = request.form.get('txtDeleteusuarioID')
+
+    sql_query = f"DELETE FROM tblUsuarios WHERE aID = {usuarioID}"
+    cursor = conn.execute(sql_query)
+    conn.commit()
+
+    response_data = jsonify({
+        'title': "Confirmação",
+        'body': "<div class='text-center text-success'><i class='fas fa-check fa-4x mb-3'></i></div><div class='text-black'>Usuario excluído com sucesso</div>"
+     })
+
+    return make_response(response_data, 200)
+
+@app.route("/requisicoes/cadastrar", methods = ['POST', 'GET'])
+def requisicoesCadastrar():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        titulo = request.form.get('txtCadastrarTitulo')
+        dataExpectativa = request.form.get('txtCadastrarDataExpectativa')
+        descricao = request.form.get('txtCadastrarDescricao')
+        observacoes = request.form.get('txtCadastrarObservacoes')
+        
+        sql_query = f"""
+        INSERT INTO tblRequisicoes(bTitulo, bDataExpectativa, bDescricao, bObservacoes) 
+        VALUES ('{titulo}', '{dataExpectativa}', '{descricao}', '{observacoes}');
+        INSERT INTO tblAcompanhamento(cRequisicaoID, cUsuarioID, cStatus, cData)
+        VALUES (last_insert_rowid(), NULL, 'NOVO', DATETIME());
+        """
+        conn.executescript(sql_query)
+        conn.commit()
+        response_data = jsonify({
+            'title': "Confirmação",
+            'body': "<div class='text-center text-success'><i class='fas fa-check fa-4x mb-3'></i></div><div class='text-black'>Requisição cadastrada com sucesso</div>"
+        })
+
+        return make_response(response_data, 200)
+    return render_template('requisicoes.cadastrar.html')
+
+@app.route("/requisicoes/novas")
+def novasRequisicoes():
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor = conn.execute("""
+        SELECT  
+            tblRequisicoes.bID,
+            tblRequisicoes.bTitulo,
+            tblAcompanhamento.cStatus,
+            strftime('%d/%m/%Y %H:%M:%S', tblAcompanhamento.cData)
+        FROM tblRequisicoes
+        LEFT JOIN tblAcompanhamento
+        ON      tblAcompanhamento.cID =
+                (
+                SELECT  cID
+                FROM    tblAcompanhamento
+                WHERE   tblRequisicoes.bID = tblAcompanhamento.cRequisicaoID
+                ORDER BY
+                        tblAcompanhamento.cID DESC
+                LIMIT   1
+                )
+        WHERE tblAcompanhamento.cStatus = 'NOVO'
+    """)
+    requisicoes = [
+        dict(id=row[0], titulo=row[1], status=row[2], data=row[3])
+        for row in cursor.fetchall()
+    ]
+    return render_template('requisicoes.novas.html', requisicoes=requisicoes)
+
+@app.route("/requisicoes/detalhes/<id>/<origem>")
+def requisicoesDetalhes(id, origem):
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor = conn.execute(f"""
+        SELECT  *
+        FROM tblRequisicoes
+        LEFT JOIN tblAcompanhamento
+        ON      tblAcompanhamento.cID =
+                (
+                SELECT  cID
+                FROM    tblAcompanhamento
+                WHERE   tblRequisicoes.bID = tblAcompanhamento.cRequisicaoID
+                ORDER BY
+                        tblAcompanhamento.cID DESC
+                LIMIT   1
+                )
+        WHERE tblRequisicoes.bID = {id}
+    """)
+    requisicao = cursor.fetchone()
+
+    cursor = conn.execute("SELECT * FROM tblUsuarios")
+    usuarios = [
+        dict(id=row[0], nome=row[1], email=row[2], contato=row[3], profissao=row[4], area=row[5])
+        for row in cursor.fetchall()
+    ]
+
+    cursor = conn.execute(f"SELECT cID, cRequisicaoID, cUsuarioID, cStatus, strftime('%d/%m/%Y %H:%M:%S', cData), aNome FROM tblAcompanhamento LEFT JOIN tblUsuarios ON tblUsuarios.aID = tblAcompanhamento.cUsuarioID WHERE cRequisicaoID = {id}")
+    historicos = [
+        dict(id=row[0], requisicaoID=row[1], usuarioID=row[2], status=row[3], data=row[4], nome=row[5])
+        for row in cursor.fetchall()
+    ]
+
+    return render_template('requisicoes.detalhes.html', requisicao=requisicao, usuarios=usuarios, historicos=historicos, origem=origem)
+
+@app.route('/requisicoes/analisar', methods = ['POST'])
+def analisarRequisicao():
+    conn = db_connection()
+    cursor = conn.cursor()
+    requisicaoID = request.form.get('txtAnalisarRequisicaoID')
+    usuarioID = request.form.get('txtAnalisarUsuarioID')
+
+    sql_query = f"INSERT INTO tblAcompanhamento(cRequisicaoID, cUsuarioID, cStatus, cData) VALUES ({requisicaoID}, {usuarioID}, 'EM ANALISE', DATETIME())"
+    cursor = conn.execute(sql_query)
+    conn.commit()
+
+    response_data = jsonify({
+        'title': "Confirmação",
+        'body': "<div class='text-center text-success'><i class='fas fa-check fa-4x mb-3'></i></div><div class='text-black'>Status atualizado com sucesso</div>"
+     })
+
+    return make_response(response_data, 200)
+
+@app.route("/requisicoes/emAnalise")
+def requisicoesEmAndamento():
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor = conn.execute("""
+        SELECT  
+            tblRequisicoes.bID,
+            tblRequisicoes.bTitulo,
+            tblAcompanhamento.cStatus,
+            strftime('%d/%m/%Y %H:%M:%S', tblAcompanhamento.cData),
+            tblUsuarios.aNome
+        FROM tblRequisicoes
+        LEFT JOIN tblAcompanhamento
+        ON  tblAcompanhamento.cID =
+            (
+            SELECT  cID
+            FROM    tblAcompanhamento
+            WHERE   tblRequisicoes.bID = tblAcompanhamento.cRequisicaoID
+            ORDER BY
+                    tblAcompanhamento.cID DESC
+            LIMIT   1
+            )
+        LEFT JOIN tblUsuarios
+        ON  tblUsuarios.aID = tblAcompanhamento.cUsuarioID  
+        WHERE tblAcompanhamento.cStatus = 'EM ANALISE'
+    """)
+
+    requisicoes = [
+        dict(id=row[0], titulo=row[1], status=row[2], data=row[3], nome=row[4])
+        for row in cursor.fetchall()
+    ]
+    return render_template('requisicoes.em.analise.html', requisicoes=requisicoes)
+
+@app.route('/requisicoes/concluir', methods = ['POST'])
+def concluirRequisicao():
+    conn = db_connection()
+    cursor = conn.cursor()
+    requisicaoID = request.form.get('txtConcluirRequisicaoID')
+    usuarioID = request.form.get('txtConcluirUsuarioID')
+
+    sql_query = f"INSERT INTO tblAcompanhamento(cRequisicaoID, cUsuarioID, cStatus, cData) VALUES ({requisicaoID}, {usuarioID}, 'CONCLUIDO', DATETIME())"
+    cursor = conn.execute(sql_query)
+    conn.commit()
+
+    response_data = jsonify({
+        'title': "Confirmação",
+        'body': "<div class='text-center text-success'><i class='fas fa-check fa-4x mb-3'></i></div><div class='text-black'>Status atualizado com sucesso</div>"
+     })
+
+    return make_response(response_data, 200)
+
+@app.route("/requisicoes/concluidas")
+def requisicoesConcluidas():
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor = conn.execute("""
+        SELECT  
+            tblRequisicoes.bID,
+            tblRequisicoes.bTitulo,
+            tblAcompanhamento.cStatus,
+            strftime('%d/%m/%Y %H:%M:%S', tblAcompanhamento.cData),
+            tblUsuarios.aNome
+        FROM tblRequisicoes
+        LEFT JOIN tblAcompanhamento
+        ON  tblAcompanhamento.cID =
+            (
+            SELECT  cID
+            FROM    tblAcompanhamento
+            WHERE   tblRequisicoes.bID = tblAcompanhamento.cRequisicaoID
+            ORDER BY
+                    tblAcompanhamento.cID DESC
+            LIMIT   1
+            )
+        LEFT JOIN tblUsuarios
+        ON  tblUsuarios.aID = tblAcompanhamento.cUsuarioID  
+        WHERE tblAcompanhamento.cStatus = 'CONCLUIDO'
+    """)
+
+    requisicoes = [
+        dict(id=row[0], titulo=row[1], status=row[2], data=row[3], nome=row[4])
+        for row in cursor.fetchall()
+    ]
+    return render_template('requisicoes.concluidas.html', requisicoes=requisicoes)
+
+@app.route('/requisicoes/editar', methods = ['POST'])
+def editarRequisicao():
+    conn = db_connection()
+    cursor = conn.cursor()
+    requisicaoID = request.form.get('txtEditarRequisicaoID')
+    titulo = request.form.get('txtEditarTituloRequisicao')
+    dataExpectativa = request.form.get('txtEditarDataExpectativa')
+    descricao = request.form.get('txtEditarDescricao')
+    observacoes = request.form.get('txtEditarObservacoes')
+
+    sql_query = f"UPDATE tblRequisicoes SET bTitulo = '{titulo}', bDataExpectativa = '{dataExpectativa}', bDescricao = '{descricao}', bObservacoes = '{observacoes}' WHERE bID = {requisicaoID}"
+    cursor = conn.execute(sql_query)
+    conn.commit()
+
+    response_data = jsonify({
+        'title': "Confirmação",
+        'body': "<div class='text-center text-success'><i class='fas fa-check fa-4x mb-3'></i></div><div class='text-black'>Requisição atualizada com sucesso</div>"
+     })
+
+    return make_response(response_data, 200)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
 
+document.addEventListener("DOMContentLoaded", function() {
+    
     const sidebar_collapse_menus = document.querySelectorAll('.open-menu');
 
     sidebar_collapse_menus.forEach(element => element.addEventListener('click', () => {
@@ -92,43 +93,286 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
 
-    $('#tblUsuarios').DataTable({
-        dom: "<'row'<'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'f>>" +
-            "<'row'<'col-sm-12'tr>>" +
-            "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
-        buttons: [{
-            extend: 'excelHtml5',
-        }],
-        columnDefs: [{
-            targets: [-1],
-            orderable: false,
-        }],
-        order: [
-            [0, "desc"]
-        ],
-        language: {
-            lengthMenu: "Mostrar _MENU_ registros por página",
-            zeroRecords: "Nada encontrado - desculpa =/",
-            info: "Total de _TOTAL_ registros",
-            infoEmpty: "Nenhum registro encontrado",
-            infoFiltered: "(Filtrado de _MAX_ registros)",
-            search: '',
-            searchPlaceholder: "Faça sua busca",
-            paginate: {
-                first: "Primeira",
-                last: "Última",
-                next: "Próxima",
-                previous: "Anterior",
-            },
-            emptyTable: "Nada para mostrar aqui!",
-        },
-        lengthMenu: [
-            [5, 10, 15, 20, 25, -1],
-            [5, 10, 15, 20, 25, "Todos"]
-        ],
-        pageLength: 25,
-        responsive: true
-    });
+    if (document.getElementById('main-listas-usuario')) {
 
+        document.querySelectorAll('.editUsuario').forEach(element => element.addEventListener('click', function() {
+            fetch('/dadosUsuario', {
+                method: "POST",
+                body: JSON.stringify({ 'txtEditUsuarioID': this.getAttribute('data-id') }),
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            })
+            .then(response => {
+                console.log(response)
+
+                return response.json();
+            })
+            .then(data => {
+                var modal = new bootstrap.Modal(document.getElementById('editar-usuario-modal'), {
+                    keyboard: false
+                });
+        
+                document.getElementById('editar-usuario-modal').querySelector('#txtEditusuarioID').value = this.getAttribute('data-id');
+                document.getElementById('editar-usuario-modal').querySelector('#txtEditarNome').value = data.nome;
+                document.getElementById('editar-usuario-modal').querySelector('#txtEditarEmail').value = data.email;
+                document.getElementById('editar-usuario-modal').querySelector('#txtEditarTelefone').value = data.telefone;
+                document.getElementById('editar-usuario-modal').querySelector('#txtEditarProfissao').value = data.profissao;
+                document.getElementById('editar-usuario-modal').querySelector('#txtEditarArea').value = data.area;
+        
+                modal.show();
+            })
+        }))
+
+        document.getElementById('editar-usuario-form').addEventListener('submit', function(event) {
+            closeModal('editar-usuario-modal')
+            event.preventDefault();
+            var form = new FormData(this);
+            postForms(this.action, this.method, form);
+        })
+
+        document.querySelectorAll('.deleteUsuario').forEach(element => element.addEventListener('click', function() {
+            var modal = new bootstrap.Modal(document.getElementById('deletar-usuario-modal'), {
+                keyboard: false
+            });
+
+            document.getElementById('deletar-usuario-modal').querySelector('#txtDeleteusuarioID').value = this.getAttribute('data-id');
+
+            modal.show()
+        }))
+
+
+        document.getElementById('deletar-usuario-form').addEventListener('submit', function(event) {
+            closeModal('deletar-usuario-modal')
+            event.preventDefault();
+            var form = new FormData(this);
+            postForms(this.action, this.method, form);
+        })
+
+        
+
+        document.getElementById('cadastrar-usuario-form').addEventListener('submit', function(event) {
+            submitButtonSpinner('btnCadastrarUsuario')
+            event.preventDefault();
+            var form = new FormData(this);
+            postForms(this.action, this.method, form);
+        })
+
+
+        $('#tblUsuarios').DataTable({
+            dom: "<'row'<'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+            buttons: [{
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            }],
+            columnDefs: [{
+                targets: [-1],
+                orderable: false,
+            }],
+            order: [
+                [0, "desc"]
+            ],
+            language: {
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                zeroRecords: "Nada encontrado - desculpa =/",
+                info: "Total de _TOTAL_ registros",
+                infoEmpty: "Nenhum registro encontrado",
+                infoFiltered: "(Filtrado de _MAX_ registros)",
+                search: '',
+                searchPlaceholder: "Faça sua busca",
+                paginate: {
+                    first: "Primeira",
+                    last: "Última",
+                    next: "Próxima",
+                    previous: "Anterior",
+                },
+                emptyTable: "Nada para mostrar aqui!",
+            },
+            lengthMenu: [
+                [5, 10, 15, 20, 25, -1],
+                [5, 10, 15, 20, 25, "Todos"]
+            ],
+            pageLength: 25,
+            responsive: true
+        });
+
+        $('#txtCadastrarContato').mask('(00) 0000-0000');
+    }
+
+    if (document.getElementById('main-listas-requisicoes-cadastrar')) {
+        document.getElementById('cadastrar-requisicao-form').addEventListener('submit', function(event) {
+            submitButtonSpinner('btnCadastrarRequisicao')
+            event.preventDefault();
+            var form = new FormData(this);
+            postForms(this.action, this.method, form);
+        })
+
+        $('#txtCadastrarDataExpectativa').mask('00/00/0000');
+    }
+
+    if (document.getElementById('main-requisicoes-detalhes')) {
+        if (document.getElementById('analisar-requisicao-modal')) {
+            document.getElementById('analisar-requisicao-form').addEventListener('submit', function(event) {
+                closeModal('analisar-requisicao-modal');
+                event.preventDefault();
+                var form = new FormData(this);
+                postForms(this.action, this.method, form);
+            })
+        }
+
+        if (document.getElementById('concluir-requisicao-modal')) {
+            document.getElementById('concluir-requisicao-form').addEventListener('submit', function(event) {
+                closeModal('concluir-requisicao-modal');
+                event.preventDefault();
+                var form = new FormData(this);
+                postForms(this.action, this.method, form);
+            })
+        }
+
+        if (document.getElementById('editar-requisicao-modal')) {
+            document.getElementById('editar-requisicao-form').addEventListener('submit', function(event) {
+                closeModal('editar-requisicao-modal');
+                event.preventDefault();
+                var form = new FormData(this);
+                postForms(this.action, this.method, form);
+            })
+
+            $('#txtEditarDataExpectativa').mask('00/00/0000');
+        }
+        
+    }
+
+    if (document.getElementById('main-requisicoes-novas')) {
+        $('#tblRequisicoesNovas').DataTable({
+            dom: "<'row'<'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+            buttons: [{
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            }],
+            columnDefs: [{
+                targets: [-1],
+                orderable: false,
+            }],
+            order: [
+                [2, "desc"]
+            ],
+            language: {
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                zeroRecords: "Nada encontrado - desculpa =/",
+                info: "Total de _TOTAL_ registros",
+                infoEmpty: "Nenhum registro encontrado",
+                infoFiltered: "(Filtrado de _MAX_ registros)",
+                search: '',
+                searchPlaceholder: "Faça sua busca",
+                paginate: {
+                    first: "Primeira",
+                    last: "Última",
+                    next: "Próxima",
+                    previous: "Anterior",
+                },
+                emptyTable: "Nada para mostrar aqui!",
+            },
+            lengthMenu: [
+                [5, 10, 15, 20, 25, -1],
+                [5, 10, 15, 20, 25, "Todos"]
+            ],
+            pageLength: 25,
+            responsive: true
+        });
+    }
+
+    if (document.getElementById('main-requisicoes-em-andamento')) {
+        $('#tblRequisicoesEmAndamento').DataTable({
+            dom: "<'row'<'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+            buttons: [{
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            }],
+            columnDefs: [{
+                targets: [-1],
+                orderable: false,
+            }],
+            order: [
+                [3, "desc"]
+            ],
+            language: {
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                zeroRecords: "Nada encontrado - desculpa =/",
+                info: "Total de _TOTAL_ registros",
+                infoEmpty: "Nenhum registro encontrado",
+                infoFiltered: "(Filtrado de _MAX_ registros)",
+                search: '',
+                searchPlaceholder: "Faça sua busca",
+                paginate: {
+                    first: "Primeira",
+                    last: "Última",
+                    next: "Próxima",
+                    previous: "Anterior",
+                },
+                emptyTable: "Nada para mostrar aqui!",
+            },
+            lengthMenu: [
+                [5, 10, 15, 20, 25, -1],
+                [5, 10, 15, 20, 25, "Todos"]
+            ],
+            pageLength: 25,
+            responsive: true
+        });
+    }
+
+    if (document.getElementById('main-requisicoes-concluidas')) {
+        $('#tblRequisicoesConcluidas').DataTable({
+            dom: "<'row'<'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+            buttons: [{
+                extend: 'excelHtml5',
+                exportOptions: {
+                    columns: ':not(:last-child)'
+                }
+            }],
+            columnDefs: [{
+                targets: [-1],
+                orderable: false,
+            }],
+            order: [
+                [3, "desc"]
+            ],
+            language: {
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                zeroRecords: "Nada encontrado - desculpa =/",
+                info: "Total de _TOTAL_ registros",
+                infoEmpty: "Nenhum registro encontrado",
+                infoFiltered: "(Filtrado de _MAX_ registros)",
+                search: '',
+                searchPlaceholder: "Faça sua busca",
+                paginate: {
+                    first: "Primeira",
+                    last: "Última",
+                    next: "Próxima",
+                    previous: "Anterior",
+                },
+                emptyTable: "Nada para mostrar aqui!",
+            },
+            lengthMenu: [
+                [5, 10, 15, 20, 25, -1],
+                [5, 10, 15, 20, 25, "Todos"]
+            ],
+            pageLength: 25,
+            responsive: true
+        });
+    }
 
 });
